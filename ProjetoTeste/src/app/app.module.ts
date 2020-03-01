a2ce103ea8897bb2ac8611f42b3b961ac8446649
Felipe Portela <felipe.portela@checkmob.com>
2020-03-01 11:21:16 -0300

    adicionado o font awesome no projeto

--- ProjetoTeste/src/app/app.module.ts
+++ ProjetoTeste/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './Navbar/navbar/navbar.component';
 import { HomeComponent } from './Home/home/home.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HomeComponent } from './Home/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
