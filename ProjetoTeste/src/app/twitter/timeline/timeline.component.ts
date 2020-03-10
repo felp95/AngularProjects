98a73eb9a62665ed6616fb0b879e65e3dc1b690a
Felipe Portela <felipe.portela@checkmob.com>
2020-03-09 21:05:48 -0300

    finalizado o curso de flex box da rocketseat

--- ProjetoTeste/src/app/twitter/timeline/timeline.component.ts
+++ ProjetoTeste/src/app/twitter/timeline/timeline.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
+import {
+  faHeart,
+  faEnvelope,
+  faComment
+} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
-  selector: 'app-timeline',
-  templateUrl: './timeline.component.html',
-  styleUrls: ['./timeline.component.css']
+  selector: "app-timeline",
+  templateUrl: "./timeline.component.html",
+  styleUrls: ["./timeline.component.css"]
 })
-export class TimelineComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class TimelineComponent {
+  faHeart = faHeart;
+  faEnvelope = faEnvelope;
+  faComment = faComment;
 }
